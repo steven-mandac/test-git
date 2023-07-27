a74403221fdab56a09fd2ac6f405a13cbd49dd9a
stevenmandac <steven.mandac15@gmail.com>
2023-07-28 00:19:12 +0800

    [Add] added isbn generator feature

--- features.py
+++ features.py
@@ -5,10 +5,10 @@ class Feature:
     
     def __init__(self):
         self.id = randint(1000, 9999)
+        self.isbn = ""
         self.author = ""
         self.title = ""
         self.volume = 0
-        self.isbn = ""
         self.status = True
         
         try:
@@ -16,7 +16,22 @@ class Feature:
         except FileNotFoundError:
             self.books = []
         else:
-            self.books = data.to_dict(orient="records")      
+            self.books = data.to_dict(orient="records")
+            
+    def isbn_generator(self):
+        self.isbn += str(randint(1, 9))
+        for _ in range(12):
+            self.isbn += str(randint(0, 9))
+        
+        dupe = True
+        while dupe:
+            for book in self.books:
+                if self.isbn == book['ISBN']:
+                    self.isbn_generator()
+                else:
+                    dupe = False
+                   
+        return self.reformat_isbn(self.isbn)
     
     def user_input(self, prompt):
         if prompt == "add":
@@ -41,44 +56,30 @@ class Feature:
                 if self.id == book['ID']:
                     self.id = randint(1000, 9999)
                     break
-        print(self.id)    
-        self.isbn = ""
-        new_isbn = ""
+                
+        self.isbn = self.isbn_generator()
+        print(f"ISBN: {self.isbn}")
         self.author = input("Author: ").title()
         self.title = input("Title: ").title()
         self.volume = int(input("Volume: "))   
-        
-        while len(self.isbn) != 13:
-            self.isbn = str(input("ISBN: "))
-            if len(self.isbn) < 13 or len(self.isbn) > 13:
-                print("Invalid ISBN: Must be 13-digit long.")
-            else:
-                new_isbn = self.reformat_isbn(self.isbn)
                 
-            for book in self.books:
-                if new_isbn == book['ISBN']:
-                    print(" This ISBN is already in use")
-                    self.isbn += "1"
-                    self.get_data()
-                    break
-                
-        self.record_data(self.id, self.author, self.title, self.volume, new_isbn)
+        self.record_data(self.id, self.isbn, self.author, self.title, self.volume)
         
-    def record_data(self, id, author, title, volume, isbn):
+    def record_data(self, id, isbn, author, title, volume):
         new_data = {
             "ID": id,
+            "ISBN": isbn,
             "Author": author,
             "Title": title,
-            "Volume": volume,
-            "ISBN": isbn
+            "Volume": volume
         }
         self.books.append(new_data)
         
     def read_data(self):
         for book in self.books:
             print(
-                "ID-%i | Author: %s | Title: %s | Volume: %i | ISBN: %s" 
-                % (book['ID'], book['Author'], book['Title'], book['Volume'], book['ISBN'])
+                "ID-%i | ISBN: %s | Author: %s | Title: %s | Volume: %i" 
+                % (book['ID'], book['ISBN'], book['Author'], book['Title'], book['Volume'])
             )
             
     def update_data(self, isbn):
@@ -91,7 +92,8 @@ class Feature:
         if book_index is None:
             print("404: Book not found.")
             self.user_input("update")
-        else:            
+        else:
+            print(f" ISBN*{self.books[book_index]['ISBN']}")
             self.books[book_index]['Author'] = input("(Update) Author: ").title()
             self.books[book_index]['Title'] = input("(Update) Title: ").title()
             self.books[book_index]['Volume'] = int(input("(Update) Volume: "))
@@ -116,8 +118,7 @@ class Feature:
             warn = input(f"You are about to remove this book\n Author: {author} | Title: {title} | Volume: {volume} | ISBN: {isbn}\n y or n: ").lower()
             if warn == 'y':
                 self.books.pop(book_index)
-                
-      
+
     def reformat_isbn(self, isbn):
         isbn_update = ""
         for _ in range(len(isbn)):
